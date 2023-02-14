f0046f3cd62c4d80c0963f4a02e53151d16bf796
DavidMeseha <111684897+DavidMeseha@users.noreply.github.com>
2023-02-14 21:32:54 +0200

    cc

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,12 @@
 import '@/styles/globals.scss'
 import { NavStateProvider } from '../context/NavStateProvider';
+import { Analytics } from '@vercel/analytics'
 
 export default function App({ Component, pageProps }) {
   return (
     <NavStateProvider>
       <Component {...pageProps} />
+      <Analytics />
     </NavStateProvider>
   )
 }
