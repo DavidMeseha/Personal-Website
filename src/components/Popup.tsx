d8baf8dd09c99c5c03e94bb44d828cb44a6622cd
David <davidmmy@gmail.com>
2024-03-04 21:18:08 +0200

    Investigate image show proplem

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -1,22 +1,22 @@
 import style from "@/styles/popup.module.scss";
 import UsePopups from "@/hooks/usePopups";
 import { Close } from "./Icons";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Image from "next/image";
 import Loading from "./Loading";
 
 export default function Popup() {
   const { graphicProject, setGraphicProject } = UsePopups();
-  const [showContent, setShowContent] = useState(false);
+  // const [showContent, setShowContent] = useState(true);
   const [imgLoaded, setImgLoaded] = useState(false);
 
-  const timeoutId = setTimeout(() => {
-    setShowContent(true);
-  }, 400);
+  // const timeoutId = setTimeout(() => {
+  //   setShowContent(true);
+  // }, 400);
 
-  useEffect(() => {
-    return clearTimeout(timeoutId);
-  }, []);
+  // useEffect(() => {
+  //   return clearTimeout(timeoutId);
+  // }, []);
 
   function closeClickHandle() {
     setGraphicProject({ show: false, img: "" });
@@ -31,8 +31,8 @@ export default function Popup() {
         >
           <Close />
         </div>
-        {(!imgLoaded || !showContent) && <Loading />}
-        {showContent && (
+        {(!imgLoaded) && <Loading />}
+        {true && (
           <>
             <Image
               className={`${imgLoaded ? style["show"] : style["hide"]}`}
