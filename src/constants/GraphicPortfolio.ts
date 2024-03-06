2f4671ff23cf3ad386491c288aa41d058ded6979
David <davidmmy@gmail.com>
2024-03-06 12:23:40 +0200

    Test wepb image

--- src/constants/GraphicPortfolio.ts
+++ src/constants/GraphicPortfolio.ts
@@ -6,39 +6,39 @@ export interface GraphicProject {
 
 export const graphicProjects: GraphicProject[] = [
   {
-    title: "Social media - African Queen",
-    img: "/photos/4head.jpg",
-    contentImg: ["/photos/4main-a.webp", "/photos/4main-b.webp"],
-  },
-  {
     title: "Social Media Art Work Vol 1 - Be-Online Agency",
-    img: "/photos/2head.jpg",
+    img: "/photos/2head.webp",
     contentImg: [
-      "/photos/2main-a.jpg",
-      "/photos/2main-b.jpg",
-      "/photos/2main-c.jpg",
-      "/photos/2main-d.jpg",
-      "/photos/2main-e.jpg",
-    ],
-  },
-  {
-    title: "Chicken Art - Social Media - Hot Chicken",
-    img: "/photos/3head.jpg",
-    contentImg: [
-      "/photos/3main-a.webp",
-      "/photos/3main-b.webp",
-      "/photos/3main-c.webp",
+      "/photos/2main-a.webp",
+      "/photos/2main-b.webp",
+      "/photos/2main-c.webp",
+      "/photos/2main-d.webp",
+      "/photos/2main-e.webp",
     ],
   },
   {
     title: "Social Media Art Work Vol 2 - Be-Online Agency",
-    img: "/photos/1head.jpg",
+    img: "/photos/1head.webp",
     contentImg: [
-      "/photos/1main-a.jpg",
-      "/photos/1main-b.jpg",
-      "/photos/1main-c.jpg",
-      "/photos/1main-d.jpg",
-      "/photos/1main-e.jpg",
+      "/photos/1main-a.webp",
+      "/photos/1main-b.webp",
+      "/photos/1main-c.webp",
+      "/photos/1main-d.webp",
+      "/photos/1main-e.webp",
+    ],
+  },
+  {
+    title: "Chicken Art - Social Media - Hot Chicken",
+    img: "/photos/4head.webp",
+    contentImg: ["/photos/4main-a.webp", "/photos/4main-b.webp"],
+  },
+  {
+    title: "Social media - African Queen",
+    img: "/photos/3head.webp",
+    contentImg: [
+      "/photos/3main-a.webp",
+      "/photos/3main-b.webp",
+      "/photos/3main-c.webp",
     ],
   },
 ];
