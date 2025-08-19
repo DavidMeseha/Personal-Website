aef0aa6820e21891b33bbbf8577b1f636aa4948a
David <davidmmyh@gmail.com>
2025-08-19 11:30:44 +0300

    Removed dates from projects

--- src/components/DevProjectCard.tsx
+++ src/components/DevProjectCard.tsx
@@ -15,7 +15,7 @@ const ProjectCard: React.FC<{
         style={{ display: "flex", justifyContent: "space-between" }}
       >
         <h1 className={style.heading}>{project.title}</h1>
-        <div className={style.dates}>
+        {/* <div className={style.dates}>
           <div style={{ marginBottom: 4 }}>
             Created :{" "}
             {new Date(project.createdAt).toLocaleDateString("en", {
@@ -30,7 +30,7 @@ const ProjectCard: React.FC<{
               year: "numeric",
             })}
           </div>
-        </div>
+        </div> */}
       </div>
       <div className={style.description}>
         <div>
